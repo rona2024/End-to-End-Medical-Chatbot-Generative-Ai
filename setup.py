1d7709fe06a7337e0ea5da2ce895d1b17b65d6fe
Your Name <ronapradhan68@gmail.com>
2025-09-25 07:46:39 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Generative AI Project",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author_email='ronapradhan68@gmail.com'
+)
